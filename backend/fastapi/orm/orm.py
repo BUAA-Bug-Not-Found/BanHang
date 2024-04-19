b2fae6db2ace61a7ecbfd77a82f266ea5a5f3a80
luyuheng <luyuheng2002325@163.com>
2024-04-19 16:55:09 +0800

    添加数据库路径自适应。当前自动部署数据库每次重置，后续上线切换为migration

--- backend/fastapi/orm/orm.py
+++ backend/fastapi/orm/orm.py
@@ -1,7 +1,14 @@
+import os
 import peewee
 import datetime
 
-db = peewee.SqliteDatabase('my_database.db')
+current_work_dir = os.path.dirname(__file__)
+
+# 如果在服务器运行，可以通过设置该环境变量确认保存的位置，如果没有设置则在fastapi目录下创建一个。
+if os.getenv("BANHANG_DATABASE_PATH") is not None:
+    db = peewee.SqliteDatabase(os.getenv("BANHANG_DATABASE_PATH"))
+else:
+    db = peewee.SqliteDatabase('{}/../my_database.db'.format(current_work_dir))
 
 from peewee import Model, CharField, IntegerField, AutoField
 
@@ -9,6 +16,7 @@ class User(Model):
     uid = AutoField(primary_key=True)
     username = CharField(max_length=32, unique=True)
     password = CharField(max_length=32)
+    admin = peewee.BooleanField(default=False)
 
     class Meta:
         database = db
@@ -16,4 +24,6 @@ class User(Model):
 
 
 if __name__ == '__main__':
+    # 暂时约定执行这部分代码的目的是重新生成整个数据库。
+    # 修改模型后，记得在此处建表
     User.create_table()
