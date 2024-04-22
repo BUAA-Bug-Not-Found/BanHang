e67894517dc4b1c4af2086cf59414058b023b2c6
潘卓然 <21371249@buaa.edu.cn>
2024-04-22 11:42:53 +0800

    Resolve merge conflict in index.js

--- vueproject/src/router/index.js
+++ vueproject/src/router/index.js
@@ -1,9 +1,16 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
 import HelpCenter from "@/components/HelpCenter/HelpCenter.vue";
-import HomeIndex from "@/components/index.vue"
+import BlogList from "@/components/AnonymousBlog/BlogList.vue";
+import BlogView from "@/components/AnonymousBlog/BlogView.vue";
+import BlogNew from "@/components/AnonymousBlog/BlogNew.vue";
+import HomeIndex from "@/components/index.vue";
 
 const routes = [
+    { path: '/helpCenter', component: HelpCenter },
+    { path: '/blogList', component: BlogList },
+    { path: '/blogView/:id', component: BlogView},
+    { path: '/blogNew', component: BlogNew},
     {
         path: '/',
         name: "index",
