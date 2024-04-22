9dc0bfa452043ca79c138b36465cbdc6ccaecfb5
luyuheng <luyuheng2002325@163.com>
2024-04-22 19:08:49 +0800

    支持修改密码接口

--- backend/fastapi/banhang/user.py
+++ backend/fastapi/banhang/user.py
@@ -20,7 +20,7 @@ router = APIRouter()
 
 
 class loginRequest(BaseModel):
-    username:str
+    email:str
     password:str
 class usernameRequest(BaseModel):
     username:str
@@ -28,10 +28,12 @@ class emailRequest(BaseModel):
     email:str
 class registerRequest(schemas.UserCreate):
     checkCode:str
+class resetPasswordRequest(loginRequest):
+    checkCode:str
 
 @router.put("/login")
 def login(req:loginRequest,response: Response, db: Session = Depends(get_db)):
-    user = crud.get_user_by_username(db, req.username)
+    user = crud.get_user_by_email(db, req.username)
     if not user or user.password != req.password:
         raise HTTPException(status_code=400, detail="Invalid username or password")
     response.set_cookie(key="Auth", value=generate_jwt_token(user.id, user.username))
@@ -56,6 +58,7 @@ def check_username_registered(current_user: Optional[dict] = Depends(authorize))
 @router.post("/sendCheckCode")
 def send_check_code(req: emailRequest, db:Session = Depends(get_db)):
     checkcode = random.choice(range(10000,100000))
+    # print(checkcode)
     if os.environ.get("CHECKCODE") is not None:
         checkcode = int(os.environ.get("CHECKCODE"))
     if not is_valid_email(req.email):
@@ -68,3 +71,13 @@ def send_check_code(req: emailRequest, db:Session = Depends(get_db)):
         return {"isSuccess":True}
     except:
         raise EXC.UniException(key = "isSuccess", value = False, others={"detail":"内部错误"})
+
+@router.post("/resetPassword")
+def reset_password(req:resetPasswordRequest, db:Session = Depends(get_db)):
+    if not crud.is_valid_checkCode(db, req.checkCode, req.email):
+        raise EXC.UniException(key = "isSuccess", value=False, others={"description":"Invalid check"})
+    user = crud.get_user_by_email(db, req.email)
+    if not user:
+        raise HTTPException(status_code=400, detail="user exists")
+    crud.set_password_by_email(db, req.password, req.email)
+    return {"response":"success"}
