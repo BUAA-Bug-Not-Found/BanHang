98ef72e0f01de665e1276efb919833fb8e45d0bf
luyuheng <luyuheng2002325@163.com>
2024-04-22 18:35:25 +0800

    feat: 添加认证方式

--- backend/fastapi/banhang/user.py
+++ backend/fastapi/banhang/user.py
@@ -1,13 +1,14 @@
 import os
+from typing import Optional
 
-from fastapi import Depends, FastAPI, HTTPException, APIRouter
+from fastapi import Depends, FastAPI, HTTPException, APIRouter, Response, Cookie
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 
 from orm import crud,schemas
 from orm.database import get_db
 
-from tools.check_user import generate_jwt_token
+from tools.check_user import generate_jwt_token,authorize
 from tools.mail import MailSender,is_valid_email
 import banhang.BanHangException as EXC
 import random
@@ -29,12 +30,12 @@ class registerRequest(schemas.UserCreate):
     checkCode:str
 
 @router.put("/login")
-def login(req:loginRequest, db: Session = Depends(get_db)):
+def login(req:loginRequest,response: Response, db: Session = Depends(get_db)):
     user = crud.get_user_by_username(db, req.username)
     if not user or user.password != req.password:
         raise HTTPException(status_code=400, detail="Invalid username or password")
-
-    return{'set-cookie': generate_jwt_token(user.id, user.username), "isSuccess":True}
+    response.set_cookie(key="Auth", value=generate_jwt_token(user.id, user.username))
+    return{"isSuccess":True}
 
 @router.post("/registerUser")
 def register(req:registerRequest, db: Session = Depends(get_db)):
@@ -46,12 +47,11 @@ def register(req:registerRequest, db: Session = Depends(get_db)):
     crud.create_user(db, schemas.UserCreate(username = req.username, password = req.password, email = req.email))
     return {"response":"success"}
 
-@router.get("/banhang/check_username_registered")
-def check_username_registered(req:usernameRequest, db:Session = Depends(get_db)):
-    user = crud.get_user_by_username(db, req.username)
-    if user:
-        return {"response":"exists"}
-    return {"response":"valid"}
+@router.get("/check_login_state")
+def check_username_registered(current_user: Optional[dict] = Depends(authorize)):
+    if current_user:
+        return current_user
+    return {"response":"invalid"}
 
 @router.post("/sendCheckCode")
 def send_check_code(req: emailRequest, db:Session = Depends(get_db)):
@@ -63,6 +63,7 @@ def send_check_code(req: emailRequest, db:Session = Depends(get_db)):
     try:
         if os.environ.get("CHECKCODE") is None:
             MailSender.send(req.email, checkcode)
+            pass
         crud.create_checkcode_record(db, schemas.EmailCheck(email = req.email, checkcode = checkcode))
         return {"isSuccess":True}
     except:
