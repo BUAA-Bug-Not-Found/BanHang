1ce91db0d50b13cd571a92ad483230feee5c7216
chlience <chlience@outlook.com>
2024-04-22 14:30:39 +0800

    feat(数据库): 迁移到 pg

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -2,14 +2,16 @@ import uvicorn
 from fastapi import FastAPI
 
 from banhang import user
-from scripts import recreate_db
+
+from orm.database import db_init, db_test
 
 app = FastAPI()
 
 app.include_router(user.router)
 
-
+def main():
+    db_test()
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
-    recreate_db.upgrade_sqlite_db()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    main()
--- backend/fastapi/orm/database.py
+++ backend/fastapi/orm/database.py
@@ -1,23 +1,10 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-current_work_dir = os.path.dirname(__file__)
-
-# 如果在服务器运行，可以通过设置该环境变量确认保存的位置，如果没有设置则在fastapi目录下创建一个。
-if os.getenv("BANHANG_DATABASE_PATH") is not None:
-    dbPath = os.getenv("BANHANG_DATABASE_PATH")
-else:
-    if os.getenv("BANHANG_TEST") is not None:
-        dbPath = '{}/../my_database_test.db'.format(current_work_dir)
-    else:
-        dbPath = '{}/../my_database.db'.format(current_work_dir)
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///{}".format(dbPath)
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -29,3 +16,21 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def db_init():
+    Base.metadata.create_all(bind=engine)
+
+def db_test():
+    try:
+        with engine.connect() as connection:
+            print("Successfully connected to the database!")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    db_init()
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+    if 'users' in tables:
+        print("The 'users' table exists.")
+    else:
+        print("The 'users' table does not exist.")
+    
