ceafd00a298e566cccfe9be3a39ea51bdca9981a
luyuheng <luyuheng2002325@163.com>
2024-04-26 10:22:34 +0800

    CROS允许列表更新为localhost

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -27,7 +27,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
