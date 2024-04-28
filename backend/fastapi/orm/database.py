01709ad477d5cf7a822c48cdeb390da9c112a479
zj <2441444156@qq.com>
2024-04-28 20:16:37 +0800

    修改为远程数据库

--- backend/fastapi/orm/database.py
+++ backend/fastapi/orm/database.py
@@ -14,20 +14,20 @@ database = "banhang"  # 数据库名称
 username = "banhang"
 password = "banhang666"
 
-# 构建 PostgreSQL 连接字符串
-SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 
 # 创建 PostgreSQL 数据库连接引擎
-# if os.getenv("BANHANG_TEST") is not None:
-#     dbPath = '{}/../my_database_test.db'.format(current_work_dir)
-#     SQLALCHEMY_DATABASE_URL = "sqlite:///{}".format(dbPath)
-# elif os.getenv("BANHANG_DATABASE_PATH") is not None:
-#     SQLALCHEMY_DATABASE_URL = os.getenv("BANHANG_DATABASE_PATH")
-#     dbPath = os.getenv("BANHANG_DATABASE_PATH")
-# else:
-#     dbPath = '{}/../my_database.db'.format(current_work_dir)
-#     SQLALCHEMY_DATABASE_URL = "sqlite:///{}".format(dbPath)
+if os.getenv("BANHANG_TEST") is not None:
+    dbPath = '{}/../my_database_test.db'.format(current_work_dir)
+    SQLALCHEMY_DATABASE_URL = "sqlite:///{}".format(dbPath)
+elif os.getenv("BANHANG_DATABASE_PATH") is not None:
+    SQLALCHEMY_DATABASE_URL = os.getenv("BANHANG_DATABASE_PATH")
+    dbPath = os.getenv("BANHANG_DATABASE_PATH")
+else:
+    dbPath = '{}/../my_database.db'.format(current_work_dir)
+    SQLALCHEMY_DATABASE_URL = "sqlite:///{}".format(dbPath)
 
+# 构建 PostgreSQL 连接字符串
+SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 Base = declarative_base()
 engine = create_engine(SQLALCHEMY_DATABASE_URL) if SQLALCHEMY_DATABASE_URL.startswith("postgresql") else \
     create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
--- backend/fastapi/scripts/recreate_db.py
+++ backend/fastapi/scripts/recreate_db.py
@@ -1,19 +1,19 @@
 import os,sys
 sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from orm.database import engine, SQLALCHEMY_DATABASE_URL
+from orm.database import dbPath,engine, SQLALCHEMY_DATABASE_URL
 from orm import models
 
 # 如果在服务器运行，可以通过设置环境变量确认保存的位置，如果没有设置则在fastapi目录下创建一个。
 
 
-# def recreate_sqlite_db(bind_engine = engine):
-#     assert is_sqlite_db()
-#     if os.path.exists(dbPath):
-#         try:
-#             os.remove(dbPath)
-#         except:
-#             raise "can't remove old db on {}".format(dbPath)
-#     models.Base.metadata.create_all(bind=bind_engine)
+def recreate_sqlite_db(bind_engine = engine):
+    assert is_sqlite_db()
+    if os.path.exists(dbPath):
+        try:
+            os.remove(dbPath)
+        except:
+            raise "can't remove old db on {}".format(dbPath)
+    models.Base.metadata.create_all(bind=bind_engine)
 
 def recreate_pg_db(bind_engine = engine):
     assert is_pg_db()
