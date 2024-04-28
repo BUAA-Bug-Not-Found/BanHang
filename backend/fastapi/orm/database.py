712ee0a7fec27d7116c0010c7cfa5c5bd3632bda
zj <2441444156@qq.com>
2024-04-28 20:19:24 +0800

    fix:get_recent_message_conversation返回值有误

--- backend/fastapi/orm/database.py
+++ backend/fastapi/orm/database.py
@@ -7,15 +7,6 @@ current_work_dir = os.path.dirname(__file__)
 
 # 如果在服务器运行，可以通过设置该环境变量确认保存的位置，如果没有设置则在fastapi目录下创建一个。
 # 目前支持pg和sqllite
-# PostgreSQL 数据库连接信息
-host = "chlience.cn"
-port = "5432"
-database = "banhang"  # 数据库名称
-username = "banhang"
-password = "banhang666"
-
-
-# 创建 PostgreSQL 数据库连接引擎
 if os.getenv("BANHANG_TEST") is not None:
     dbPath = '{}/../my_database_test.db'.format(current_work_dir)
     SQLALCHEMY_DATABASE_URL = "sqlite:///{}".format(dbPath)
@@ -26,8 +17,6 @@ else:
     dbPath = '{}/../my_database.db'.format(current_work_dir)
     SQLALCHEMY_DATABASE_URL = "sqlite:///{}".format(dbPath)
 
-# 构建 PostgreSQL 连接字符串
-SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 Base = declarative_base()
 engine = create_engine(SQLALCHEMY_DATABASE_URL) if SQLALCHEMY_DATABASE_URL.startswith("postgresql") else \
     create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
