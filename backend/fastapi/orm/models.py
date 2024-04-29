ed3d03353211c2087a53d1a83db00265001ae534
chlience <chlience@outlook.com>
2024-04-29 13:44:03 +0800

    fix(后端): 为 Message 相关数据库调整列属性

--- backend/fastapi/orm/models.py
+++ backend/fastapi/orm/models.py
@@ -196,8 +196,8 @@ class Message(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     sender_id = Column(Integer, ForeignKey('users.id'), nullable=False) 
     receiver_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    content = Column(String)
-    create_at = Column(DateTime, server_default=func.now())
+    content = Column(String, nullable=False, default="")
+    create_at = Column(DateTime, nullable=False, server_default=func.now())
 
     sender = relationship("User", foreign_keys=[sender_id])
     receiver = relationship("User", foreign_keys=[receiver_id])
@@ -207,20 +207,18 @@ class Conversation(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     host_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     guest_user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    is_read = Column(Boolean, default=True)
-    update_at = Column(DateTime, server_default=func.now())
+    is_read = Column(Boolean, nullable=False, default=True)
+    update_at = Column(DateTime, nullable=False, server_default=func.now())
 
     host_user = relationship("User", foreign_keys=[host_user_id])
     guest_user = relationship("User", foreign_keys=[guest_user_id])
     messages = relationship("ConversationMessage", back_populates="conversation")
 
-from sqlalchemy.orm import Mapped
-
 class ConversationMessage(Base):
     __tablename__ = 'conversation_messages'
     conversation_id = Column(Integer, ForeignKey('conversations.id'), primary_key=True)
     message_id = Column(Integer, ForeignKey('messages.id'), primary_key=True)
-    is_read = Column(Boolean, default=False)
+    is_read = Column(Boolean, nullable=False, default=False)
 
     message = relationship("Message", foreign_keys=[message_id])
     conversation = relationship("Conversation", back_populates="messages", foreign_keys=[conversation_id])
